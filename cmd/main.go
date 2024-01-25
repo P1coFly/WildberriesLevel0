063fac47017f0d5c43b1d542a293c76452baf544
Artem <artem3m@mail.ru>
2024-01-25 19:48:54 +0300

    added interaction with postgres

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	modelsDB "myproject/internal/db"
+	models "myproject/internal/models"
+
+	_ "github.com/lib/pq"
 	stan "github.com/nats-io/stan.go"
 )
 
 func main() {
 
 	log.Println("Start program")
+
+	// Подключение к бд
+	connStr := "user=wb_service password=12345678 dbname=WB sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	// Параметры подключения к NATS Streaming
 	clusterID := "orders-cluster"
 	clientID := "json-consumer"
@@ -28,7 +43,14 @@ func main() {
 	// Подписка на канал
 	sub, err := sc.Subscribe(channelName, func(msg *stan.Msg) {
 		// Обработка полученного JSON
-		log.Printf("Received message: %s\n", string(msg.Data))
+		var order models.Order
+
+		if err := json.Unmarshal(msg.Data, &order); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Received message: %+v\n", order)
+		modelsDB.AddOrder(db, order)
+
 	})
 	if err != nil {
 		log.Fatal(err)
---
+++ internal/db/db.go
@@ -0,0 +1,67 @@
+package db_module
+
+import (
+	"database/sql"
+	"log"
+	"myproject/internal/models"
+)
+
+func AddOrder(db *sql.DB, order models.Order) {
+	var idxDelivary = addDelivary(db, order.Delivery)
+	log.Println(idxDelivary)
+
+	addPayment(db, order.Payment)
+
+	_, err := db.Exec(`INSERT INTO public."ORDERS" (order_uid,track_number,entry,locale,internal_signature,
+	customer_id,delivery_service,shardkey,sm_id,date_created,oof_shard,delivery_id)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
+		order.OrderUID, order.TrackNumber, order.Entry, order.Locale, order.InternalSignature, order.CustomerID,
+		order.DeliveryService, order.ShardKey, order.SmID, order.DateCreated, order.OofShard, idxDelivary)
+	if err != nil {
+		panic(err)
+	}
+
+	addItems(db, order.Items)
+
+}
+
+func addDelivary(db *sql.DB, delivery models.Delivery) int {
+	var id int
+
+	err := db.QueryRow(`INSERT INTO public."DELIVERIES" (name, phone, zip, city, address, region, email) 
+	VALUES ($1, $2, $3, $4, $5, $6, $7) returning id`,
+		delivery.Name, delivery.Phone, delivery.Zip, delivery.City, delivery.Address, delivery.Region,
+		delivery.Email).Scan(&id)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+func addPayment(db *sql.DB, payment models.Payment) {
+
+	_, err := db.Exec(`INSERT INTO public."PAYMENTS" (transaction,request_id,currency,provider,amount,payment_dt,bank,delivery_cost,goods_total,custom_fee)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+		payment.Transaction, payment.RequestID, payment.Currency, payment.Provider, payment.Amount, payment.PaymentDT,
+		payment.Bank, payment.DeliveryCost, payment.GoodsTotal, payment.CustomFee)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func addItems(db *sql.DB, items []models.Item) {
+	log.Println(items)
+
+	for _, item := range items {
+
+		_, err := db.Exec(`INSERT INTO public."ITEMS" (chrt_id,track_number,price,rid,name,sale,size,total_price,nm_id,brand,status)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
+			item.ChrtID, item.TrackNumber, item.Price, item.RID, item.Name, item.Sale,
+			item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
+		if err != nil {
+			panic(err)
+		}
+
+	}
+}
