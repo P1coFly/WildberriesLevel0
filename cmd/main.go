2e7ed2fddc2aa3fda8d09970662992fcf4f55660
Artem <artem3m@mail.ru>
2024-01-29 22:53:21 +0300

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -4,9 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
+	"net/http"
 
 	modelsDB "myproject/internal/db"
 	models "myproject/internal/models"
@@ -15,6 +13,8 @@ import (
 	stan "github.com/nats-io/stan.go"
 	"github.com/patrickmn/go-cache"
 )
+
+var orderCache *cache.Cache
 
 func main() {
 
@@ -27,10 +27,13 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+	log.Println("Connection to the database - successful")
 
-	orderCache := cache.New(-1, -1)
-
-	initializingCache(orderCache, db)
+	//инициализируем экземпляр объекта кеш
+	orderCache = cache.New(-1, -1)
+	log.Println("----Starting cache initialization from the database----")
+	initializingCache(db)
+	log.Printf("----%v records have been added to the cache from the database----\n", len(orderCache.Items()))
 
 	// Параметры подключения к NATS Streaming
 	clusterID := "orders-cluster"
@@ -50,43 +53,54 @@ func main() {
 		// Обработка полученного JSON
 		var order models.Order
 
-		if err := json.Unmarshal(msg.Data, &order); err != nil {
-			log.Fatal(err)
+		//Десериализация полученных данных
+		err := json.Unmarshal(msg.Data, &order)
+		if err != nil {
+			log.Println("Invalid  json data")
+			return
 		}
-		log.Printf("Received message: %+v\n", order)
-		modelsDB.AddOrder(db, order)
+		//пытаемся добавить заказ в бд, если получилось, то и в кеш
+		log.Printf("Trying to add an order with uid: %v to the db and to the cache\n", order.OrderUID)
+		err = modelsDB.AddOrder(db, order)
+		if err != nil {
+			log.Printf("Failed to add an order to the db: %v\n", err)
+			return
+		}
 		orderCache.Set(order.OrderUID, order, cache.NoExpiration)
-
-		ord, found := orderCache.Get(order.OrderUID)
-		if found {
-			log.Print("FROM CACHE:")
-			log.Println(ord)
-		}
+		log.Printf("Order with uid: %v was added\n", order.OrderUID)
 
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer sub.Unsubscribe()
 	defer sub.Close()
 
-	log.Println("The connection and subscription were successful")
+	log.Println("The connection and subscription  to nats-streaming server - successful")
 	log.Println("Waiting for a message...")
 
-	// Ожидание сигналов завершения работы программы
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
+	//подниммаем http-сервер
+	log.Print("Server start listening on localhost:8080")
+	fileServer := http.FileServer(http.Dir("../static/"))
+	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+	http.HandleFunc("/order", orderHandler)
 
-	log.Println("Exiting...")
+	err = http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
-func initializingCache(orderCache *cache.Cache, db *sql.DB) {
+func initializingCache(db *sql.DB) {
+	//получаем срез uid
 	uids, err := modelsDB.GetUids(db)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	//получаем заказ по uid и добавляем кго в кеш
 	for _, uid := range uids {
 		order, err := modelsDB.GetOrderByUID(db, uid)
 		if err != nil {
@@ -94,15 +108,46 @@ func initializingCache(orderCache *cache.Cache, db *sql.DB) {
 			return
 		}
 
-		log.Println(order)
-
+		log.Printf("Trying to add an order with uid: %v to the cache\n", order.OrderUID)
 		orderCache.Set(order.OrderUID, order, cache.NoExpiration)
-
-		ord, found := orderCache.Get(order.OrderUID)
-		if found {
-			log.Print("FROM CACHE:")
-			log.Println(ord)
-		}
+		log.Printf("Order with uid: %v was added\n", order.OrderUID)
 	}
+
+}
+
+func orderHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getOrder(w, r)
+	case http.MethodPost:
+		postOrder(w, r)
+	default:
+		http.Error(w, "invalid http method", http.StatusMethodNotAllowed)
+	}
+}
+
+func getOrder(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../templates/index.html")
+}
+
+func postOrder(w http.ResponseWriter, r *http.Request) {
+
+	var uid string
+	err := json.NewDecoder(r.Body).Decode(&uid)
+	log.Printf("Request to receive an order with a Uid: %v\n", uid)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//Достаём из кеша заказ по uid
+	ord, found := orderCache.Get(uid)
+	if !found {
+		http.Error(w, "Order not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(ord)
 
 }
--- internal/db/db.go
+++ internal/db/db.go
@@ -5,62 +5,83 @@ import (
 	"myproject/internal/models"
 )
 
-func AddOrder(db *sql.DB, order models.Order) {
-	var idxDelivary = addDelivary(db, order.Delivery)
+// AddOrder добавляет в бд сущность заказа
+// Если происходит ошибка при добавлении данных в таблицы Payment, Items, Delivery, или Orders,
+// функция возвращает эту ошибку. В противном случае возвращается nil.
+func AddOrder(db *sql.DB, order models.Order) error {
 
-	addPayment(db, order.Payment)
+	//Добавляем  данные в таблицу Payment
+	err := addPayment(db, order.Payment)
+	if err != nil {
+		return err
+	}
 
-	_, err := db.Exec(`INSERT INTO public."ORDERS" (order_uid,track_number,entry,locale,internal_signature,
+	//Добавляем  данные в таблицу Delivery
+	var idxDelivery int
+	idxDelivery, err = addDelivery(db, order.Delivery)
+	if err != nil {
+		return err
+	}
+	//Добавляем  данные в таблицу Orders
+	_, err = db.Exec(`INSERT INTO public."ORDERS" (order_uid,track_number,entry,locale,internal_signature,
 	customer_id,delivery_service,shardkey,sm_id,date_created,oof_shard,delivery_id)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
 		order.OrderUID, order.TrackNumber, order.Entry, order.Locale, order.InternalSignature, order.CustomerID,
-		order.DeliveryService, order.ShardKey, order.SmID, order.DateCreated, order.OofShard, idxDelivary)
+		order.DeliveryService, order.ShardKey, order.SmID, order.DateCreated, order.OofShard, idxDelivery)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	addItems(db, order.Items)
+	//Добавляем  данные в таблицу Items
+	err = addItems(db, order.Items)
+	return err
 
 }
 
-func addDelivary(db *sql.DB, delivery models.Delivery) int {
+// addDelivary добавляет данные в таблицу Delivery
+// Возвращает идентификатор добавленной записи и ошибку.
+func addDelivery(db *sql.DB, delivery models.Delivery) (int, error) {
 	var id int
 
 	err := db.QueryRow(`INSERT INTO public."DELIVERIES" (name, phone, zip, city, address, region, email) 
 	VALUES ($1, $2, $3, $4, $5, $6, $7) returning id`,
 		delivery.Name, delivery.Phone, delivery.Zip, delivery.City, delivery.Address, delivery.Region,
 		delivery.Email).Scan(&id)
-	if err != nil {
-		panic(err)
-	}
 
-	return id
+	return id, err
 }
 
-func addPayment(db *sql.DB, payment models.Payment) {
+// addPayment добавляет данные в таблицу Payment
+// Если происходит ошибка, то возвращает эту ошибку
+func addPayment(db *sql.DB, payment models.Payment) error {
 	_, err := db.Exec(`INSERT INTO public."PAYMENTS" (transaction,request_id,currency,provider,amount,payment_dt,bank,delivery_cost,goods_total,custom_fee)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
 		payment.Transaction, payment.RequestID, payment.Currency, payment.Provider, payment.Amount, payment.PaymentDT,
 		payment.Bank, payment.DeliveryCost, payment.GoodsTotal, payment.CustomFee)
-	if err != nil {
-		panic(err)
-	}
+
+	return err
 }
 
-func addItems(db *sql.DB, items []models.Item) {
+// addItems добавляет данные в таблицу Items
+// Если происходит ошибка, то возвращает эту ошибку
+func addItems(db *sql.DB, items []models.Item) error {
+	var err error
 	for _, item := range items {
 
-		_, err := db.Exec(`INSERT INTO public."ITEMS" (chrt_id,track_number,price,rid,name,sale,size,total_price,nm_id,brand,status)
+		_, err = db.Exec(`INSERT INTO public."ITEMS" (chrt_id,track_number,price,rid,name,sale,size,total_price,nm_id,brand,status)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
 			item.ChrtID, item.TrackNumber, item.Price, item.RID, item.Name, item.Sale,
 			item.Size, item.TotalPrice, item.NmID, item.Brand, item.Status)
 		if err != nil {
-			panic(err)
+			return err
 		}
 
 	}
+	return err
 }
 
+// GetUids возвращает срез Uids из бд
+// Если происходит ошибка, то возвращает эту ошибку
 func GetUids(db *sql.DB) ([]string, error) {
 	var uids []string
 
@@ -83,6 +104,8 @@ func GetUids(db *sql.DB) ([]string, error) {
 
 }
 
+// GetOrderNyUID возвращает заказ по Uid
+// Если происходит ошибка, то возвращает эту ошибку
 func GetOrderByUID(db *sql.DB, uid string) (models.Order, error) {
 	var order models.Order
 
@@ -128,13 +151,15 @@ func GetOrderByUID(db *sql.DB, uid string) (models.Order, error) {
 		return order, err
 	}
 
-	order.Items, err = GetItemsByTrackNumber(db, order.TrackNumber)
+	order.Items, err = getItemsByTrackNumber(db, order.TrackNumber)
 
 	return order, err
 
 }
 
-func GetItemsByTrackNumber(db *sql.DB, trackNumber string) ([]models.Item, error) {
+// getItemsByTrackNumber возвращает срез Item для конкретного закза по TrackNumber
+// Если происходит ошибка, то возвращает эту ошибку
+func getItemsByTrackNumber(db *sql.DB, trackNumber string) ([]models.Item, error) {
 	var items []models.Item
 
 	rows, err := db.Query(`SELECT
